9c8b5cb8416dd47f27dcadc5e58fec450e6a59a4
Md Amirul Islam <amirulkhan.khulna.bd@gmail.com>
2021-09-01 21:27:34 +0600

    first stap done

---
+++ js/banking.js
@@ -0,0 +1,57 @@
+document.getElementById('deposit-btn').addEventListener('click', function(){
+// total deposite field
+const depositAmountField = document.getElementById('total-deposit');
+const depositAmountText = depositAmountField.innerText;
+const depositAmount = parseFloat(depositAmountText)
+
+//deposite input field
+const depositInputField = document.getElementById('deposit-input');
+const depositInputText = depositInputField.value;
+const newdepositInput = parseFloat(depositInputText);
+
+// newTotalDepositeField 
+const newTotalDeposit = depositAmount + newdepositInput;
+depositAmountField.innerText = newTotalDeposit
+
+// balance update works 
+const balanceAmountField = document.getElementById('total-balance');
+
+const balanceAmountFieldText = balanceAmountField.innerText;
+const balanceAmount = parseFloat(balanceAmountFieldText);
+
+const newBalanceAmount = balanceAmount + newdepositInput;
+balanceAmountField.innerText = newBalanceAmount;
+
+depositInputField.value = '';
+})
+
+
+// withdraw update field  //
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+
+//total withdraw field 
+const withdrawAmountField = document.getElementById('total-withdraw');
+const withdrawAmountFieldText = withdrawAmountField.innerText;
+const withdrawAmount = parseFloat(withdrawAmountFieldText);
+
+// withdraw input field
+const withdrawInputField= document.getElementById('withdraw-input');
+const withdrawInputFieldText = withdrawInputField.value;
+const withdrawInput = parseFloat(withdrawInputFieldText);
+
+//newWithdrawBalance
+const newWithdrawBalance =  withdrawAmount + withdrawInput ;
+withdrawAmountField.innerText = newWithdrawBalance;
+
+
+
+// balane update works 
+const balanceAmountField = document.getElementById('total-balance');
+const balanceAmountFieldText = balanceAmountField.innerText;
+const balanceAmount = parseFloat(balanceAmountFieldText);
+const newBalanceAmount = balanceAmount - withdrawInput;
+balanceAmountField.innerText = newBalanceAmount;
+
+withdrawInputField.value = '';
+
+})
